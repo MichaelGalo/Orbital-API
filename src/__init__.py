2be79d606cd3638ab7aaad7a61b1e04125681705
Michael Galo <michael@michaelthinks.co>
2025-09-10 13:10:27 -0500

    updated setup.py for new temp catalog attachment

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .logger import setup_logging
-from .utils import duckdb_con_init, ducklake_init, connection_gcp_credentials, get_datasets_list, fetch_single_dataset
+from .utils import duckdb_con_init, ducklake_init, connection_gcp_credentials, get_datasets_list, fetch_single_dataset, get_local_ducklake_catalog
 
 __all__ = [
     "setup_logging",
@@ -7,5 +7,6 @@ __all__ = [
     "ducklake_init",
     "connection_gcp_credentials",
     "get_datasets_list",
-    "fetch_single_dataset"
+    "fetch_single_dataset",
+    "get_local_ducklake_catalog"
 ]
--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 import os
 from src.logger import setup_logging
-from src.utils import duckdb_con_init, ducklake_init, connection_gcp_credentials
+from src.utils import duckdb_con_init, ducklake_init, connection_gcp_credentials, get_local_ducklake_catalog
 from dotenv import load_dotenv
 
 logger = setup_logging()
@@ -9,7 +9,7 @@ load_dotenv()
 def setup():
     logger.info("Starting Orbital data lakehouse setup")
     gcp_bucket = os.getenv('GCP_BUCKET_NAME')
-    catalog_path = f"gs://{gcp_bucket}/catalog.ducklake"
+    catalog_path = get_local_ducklake_catalog()
     data_path = f"gs://{gcp_bucket}/CATALOG_DATA_SNAPSHOTS"
 
     con = duckdb_con_init()
@@ -20,6 +20,3 @@ def setup():
 
 if __name__ == "__main__":
     setup()
-
-
-#TODO: Store the pipeline catalog in GCS and simply access it from here.
